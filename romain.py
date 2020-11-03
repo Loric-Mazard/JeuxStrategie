02fd5429842ea0a608e17718b285d1041fbe4e1f
Loric-Mazard <loric.mazard@gmail.com>
2020-11-03 13:24:47 +0100

    Commentaires

--- romain.py
+++ romain.py
@@ -11,24 +11,34 @@ class Map:
 
         self.bats = []
 
+        self.money = 0
+
     def move_left(self):
         if self.rect.x < 0:
             self.rect.x += 10
+            for i in self.bats:
+                i.MajPos(10,0)
 
     def move_right(self):
         if self.rect.x > -self.rect[2] + 1080:
             self.rect.x -= 10
+            for i in self.bats:
+                i.MajPos(-10,0)
 
     def move_up(self):
         if self.rect.y < 0:
             self.rect.y += 10
+            for i in self.bats:
+                i.MajPos(0,10)
 
     def move_down(self):
         if self.rect.y > -self.rect[3] + 1080:
             self.rect.y -= 10
+            for i in self.bats:
+                i.MajPos(0,-10)
 
     def add_new_bat(self, mouse):
-        self.bats.append(Batiment(mouse)) #Pas terrible
+        self.bats.append(Batiment(mouse))
 
     def get_nb_bats(self):
         return len(self.bats)
@@ -36,7 +46,8 @@ class Map:
     def affichage(self, window):
         window.blit(self.image, self.rect)
         for i in self.bats:
-            window.blit(i.image, i.rect)
+            i.Affichage()
+        
 
 #Classe des batiments
 class Batiment:
@@ -46,6 +57,13 @@ class Batiment:
         self.rect = self.image.get_rect()
         self.rect.x = mouse[0]+10
         self.rect.y = mouse[1]+10
+
+    def Affichage(self):
+        window.blit(self.image, self.rect)
+
+    def MajPos(self, x, y):
+        self.rect.x += x
+        self.rect.y += y
 
 
 window = pygame.display.set_mode((1080, 720))
