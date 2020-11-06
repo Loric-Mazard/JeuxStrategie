16462b0b2cb767efc58b4493038a7e2966aa2e0e
Loric-Mazard <loric.mazard@gmail.com>
2020-11-06 15:08:03 +0100

    Commentaires

--- script.py
+++ script.py
@@ -3,74 +3,88 @@ import pygame
 #Classe de la map
 class Map:
 
+    #Initialisation des variables
     def __init__(self):
+        #Carte positions
         self.image = pygame.image.load("decor/map.png")
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
 
+        #Liste des batiments
         self.bats = []
 
+        #Variable des ressources
         self.money = 0
 
+    #Deplacement gauche
     def move_left(self):
         if self.rect.x < 0:
             self.rect.x += 10
             for i in self.bats:
                 i.MajPosition(10,0)
 
+    #Deplacement droite
     def move_right(self):
         if self.rect.x > -self.rect[2] + 1080:
             self.rect.x -= 10
             for i in self.bats:
                 i.MajPosition(-10,0)
 
+    #Deplacement haut
     def move_up(self):
         if self.rect.y < 0:
             self.rect.y += 10
             for i in self.bats:
                 i.MajPosition(0,10)
 
+    #Deplacement bas
     def move_down(self):
         if self.rect.y > -self.rect[3] + 1080:
             self.rect.y -= 10
             for i in self.bats:
                 i.MajPosition(0,-10)
 
+    #Ajout d'un batiment
     def add_new_bat(self, mouse):
         self.bats.append(Batiment(mouse))
 
-    def get_nb_bats(self):
-        return len(self.bats)
-
+    #Affichage dans la fenetre
     def affichage(self, window):
+        #Affichage carte
         window.blit(self.image, self.rect)
 
+        #Affichage batiments
         for i in self.bats:
             i.Affichage()
 
+        #Affichage money 
         font = pygame.font.SysFont('Lato',40,True)
         message = font.render("Money : " + str(self.money),True,(255,255,255))
         window.blit(message, (20,10,100,50))
 
+
 #Classe des batiments
 class Batiment:
 
+    #Initialisation des varaibles
     def __init__(self, mouse):
-        print(mouse)
+        #Position du batiment
         self.image = pygame.image.load("decor/batiment.png")
         self.rect = self.image.get_rect()
         self.rect.x = mouse[0]+10
         self.rect.y = mouse[1]+10
 
+    #Affichage du batiment
     def Affichage(self):
         window.blit(self.image, self.rect)
 
+    #Mise a jour de la position du batiment
     def MajPosition(self, x, y):
         self.rect.x += x
         self.rect.y += y
 
-
+#Initialisation de la fenetre
 pygame.init()
 window = pygame.display.set_mode((1080, 720))
 
@@ -89,8 +103,6 @@ while game:
             #Fermer la fenetre quand on appuis sur echap
             if event.key == pygame.K_ESCAPE:
                 game = False
-            elif event.key == pygame.K_RETURN:
-                print(map.get_nb_bats())
 
         #Creer un batiment quand on clique 
         if event.type == pygame.MOUSEBUTTONDOWN:
